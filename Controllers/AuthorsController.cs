0b60b5d0fb9d044a1e42efcc78bc6e3766156dd8
ChrystianC <chrystian.cle.cracow@gmail.com>
2023-01-11 22:29:03 +0100

    Add AuthorsController

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectASP.NET_14040.Data;
+
+namespace ProjectASP.NET_14040.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly BookStoreDbContext _context;
+
+        public AuthorsController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Authors.ToList();
+
+            return View();
+        }
+    }
+}
