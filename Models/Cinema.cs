f8dc508665d7d54a2b1213348c3624fbc386e60d
ChrystianC <chrystian.cle.cracow@gmail.com>
2023-01-11 18:45:26 +0100

    Add Cinema model

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -1,6 +1,20 @@
-﻿namespace ProjectASP.NET_14040.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectASP.NET_14040.Models
 {
     public class Cinema
     {
+        [Key]
+        public int Id { get; set; }
+
+
+        public string CinemaLogo { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public string Description { get; set; }
+
     }
 }
