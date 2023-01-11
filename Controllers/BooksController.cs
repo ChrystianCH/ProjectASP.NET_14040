6abdd2f30f1432c2eb8888809ccd05e3b833e911
ChrystianC <chrystian.cle.cracow@gmail.com>
2023-01-11 22:30:45 +0100

    Add BooksController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectASP.NET_14040.Data;
+
+namespace ProjectASP.NET_14040.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookStoreDbContext _context;
+
+        public BooksController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Books.ToList();
+
+            return View();
+        }
+    }
+}
