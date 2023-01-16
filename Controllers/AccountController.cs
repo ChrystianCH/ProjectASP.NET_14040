bf246f8b574eb542241396f01b42dd688fe43647
ChrystianC <chrystian.cle.cracow@gmail.com>
2023-01-16 21:23:00 +0100

    Add shared view _Identity

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProjectASP.NET_14040.Data;
+using ProjectASP.NET_14040.Data.Static;
 using ProjectASP.NET_14040.Data.ViewModels;
 using ProjectASP.NET_14040.Models;
 
@@ -38,7 +39,7 @@ namespace ProjectASP.NET_14040.Controllers
                     var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
                     if (result.Succeeded)
                     {
-                        return RedirectToAction("Index", "Movies");
+                        return RedirectToAction("Index", "Books");
                     }
                 }
                 TempData["Error"] = "Wrong credentials. Please, try again!";
@@ -50,5 +51,36 @@ namespace ProjectASP.NET_14040.Controllers
         }
         
         public IActionResult Register() => View(new RegisterVM());
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Books");
+        }
     }
 }
