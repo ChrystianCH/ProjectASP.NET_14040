1c3f42ec85d3e01f6a2fcbdbf613e67bc3330ff9
ChrystianC <chrystian.cle.cracow@gmail.com>
2023-01-16 20:55:29 +0100

    Add accountContllorer

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectASP.NET_14040.Data;
+using ProjectASP.NET_14040.Models;
+
+namespace ProjectASP.NET_14040.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly BookStoreDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BookStoreDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
